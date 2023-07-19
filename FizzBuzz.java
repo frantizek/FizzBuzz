7ef2a2ca632aa01b57ccdeab952448f0724ae802
José Francisco Ruvalcaba Castañeda <32653773+frantizek@users.noreply.github.com>
2023-07-19 12:30:44 -0600

    Create FizzBuzz.java

---
+++ FizzBuzz.java
@@ -0,0 +1,22 @@
+public class MyClass {
+    public static void main(String args[]) {
+        String F="Fizz";      
+        String B="Buzz";
+      
+        int i = 1;
+        
+        for(i=1; i<101; i++){
+            
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(F+B);
+            } else if (i % 3 == 0) {
+                System.out.println(F);
+            } else if (i % 5 == 0) {
+                System.out.println(B);
+            } else {
+                System.out.println(i);
+            }
+        }
+        
+    }
+}
